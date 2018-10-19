ecca2790b2079edaee0110d39cde46d7c771f9fd
Chris Badahdah <a.s.badahdah@gmail.com>
2018-10-19 14:28:06 -0700

    Pass commands to vim on startup

--- src/vrepl/core.clj
+++ src/vrepl/core.clj
@@ -8,9 +8,6 @@
     (.addShutdownHook (Runtime/getRuntime) (Thread. #(.destroy ps)))
     ps))
 
-(defn remote-send! [vi msg]
-  (.waitFor (apply sh (concat vi ["--remote-send" msg]))))
-
 (defn has-bin? [bin]
   (->> (s/split (System/getenv "PATH") #":")
        (map io/file)
@@ -18,13 +15,10 @@
        (some #(and (= (.getName %) bin) (.canExecute %)))))
 
 (defn -main [& args]
-  (let [id (name (gensym))
-        vi (if (has-bin? "mvim")
-             ["mvim" "-f" "--servername" id]
-             ["vim" "--servername" id])
-        ps (apply sh (concat vi args))
-        port (:port (nrepl/start-server))]
-    (Thread/sleep 100)
-    (remote-send! vi ":set ft=clojure<CR>")
-    (remote-send! vi (str ":Connect nrepl://localhost:" port "<CR><CR>"))
+  (let [vi (if (has-bin? "mvim") ["mvim" "-f"] ["vim"])
+        port (:port (nrepl/start-server))
+        ex ["+set ft=clojure"
+            (str "+Connect nrepl://localhost:" port " .")
+            "+insert"]
+        ps (apply sh (concat vi ex args))]
     (System/exit (.waitFor ps))))
