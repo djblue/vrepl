45d2e946fa5faff54556e097ba51298b2b341847
Chris Badahdah <a.s.badahdah@gmail.com>
2020-12-01 21:33:40 -0700

    Put some portal into vrepl

--- src/vrepl/core.clj
+++ src/vrepl/core.clj
@@ -1,25 +1,49 @@
 (ns vrepl.core
-  (:require [clojure.string :as s]
-            [clojure.java.io :as io]
+  (:require [cider.nrepl :refer [cider-nrepl-handler]]
             [nrepl.server :as nrepl]
-            clojure.repl))
+            [ns-tracker.core :as tracker]
+            [portal.api :as p]))
 
-(defn sh [& args]
+; https://cmdrdats.wordpress.com/2012/08/14/auto-reloading-code-with-clojure/
+
+(defn- check-namespace-changes [track]
+  (try
+    (doseq [ns-sym (track)]
+      (require ns-sym :reload))
+    (catch Throwable e (.printStackTrace e)))
+  (Thread/sleep 500))
+
+(defn- start-nstracker []
+  (let [track (tracker/ns-tracker ["src" "test" "resources"])]
+    (doto
+     (Thread.
+      #(while true
+         (check-namespace-changes track)))
+      (.setDaemon true)
+      (.start))))
+
+(def p nil)
+
+(defn- start-portal []
+  (alter-var-root #'p (fn [_] (p/open)))
+  (p/tap)
+  (.addShutdownHook (Runtime/getRuntime) (Thread. #(p/close))))
+
+(defn- start-nrepl []
+  (:port (nrepl/start-server :handler cider-nrepl-handler)))
+
+(defn- sh [args]
   (let [ps (.. (ProcessBuilder. args) inheritIO start)]
     (.addShutdownHook (Runtime/getRuntime) (Thread. #(.destroy ps)))
     ps))
 
-(defn has-bin? [bin]
-  (->> (s/split (System/getenv "PATH") #":")
-       (map io/file)
-       (mapcat file-seq)
-       (some #(and (= (.getName %) bin) (.canExecute %)))))
+(defn- start-vim [port args]
+  (let [ex ["+set ft=clojure"
+            (str "+Connect localhost:" port " .")
+            "+insert"]]
+    (sh (concat ["vim"] ex args))))
 
 (defn -main [& args]
-  (let [vi (if (has-bin? "mvim") ["mvim" "-f"] ["vim"])
-        port (:port (nrepl/start-server))
-        ex ["+set ft=clojure"
-            (str "+Connect localhost:" port " .")
-            "+insert"]
-        ps (apply sh (concat vi ex args))]
-    (System/exit (.waitFor ps))))
+  (start-nstracker)
+  (start-portal)
+  (-> (start-nrepl) (start-vim args) .waitFor System/exit))
